cb4a7eee399ea69aed960882e353d894e6351671
Mostafizurfsw <Mostafizurfsw@users.noreply.github.com>
2021-04-28 21:06:15 +0600

    react basic core concepts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+const friends = ['Suman', 'Shovon', 'Zillur', 'Fatin', 'zim'];
+const foods = [
+  {name: 'Beef', price: '550/kg'},
+  {name: 'Mutton', price: '800/kg'},
+  {name: 'Chicken', price: '480/kg'},
+  {name: 'tomato', price: '50/kg'},
+  {name: 'watermelon', price: '20/kg'},
+  
+]
+
   var name="Mostafizur Rahman";
   var age="25";
   var job="bekar";
@@ -21,7 +32,24 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>My name is {name}.</h1>
+      <Counter></Counter>
+      <Users></Users>
+      <div>
+        <ol>
+            {
+              friends.map(friend => <li>{friend}</li> )
+            }
+            {
+              foods.map(food => <li>{food.name} {food.price}</li> )
+            }
+            {
+              foods.map(fd => <Food food={fd}></Food> )
+            }
+          </ol>
+      </div>
+      
+      <div>
+        <h1>My name is {name}.</h1>
       <h2>My age is {age} years old.</h2>
       <h3>I am a {job}.</h3>
       <h2>
@@ -38,11 +66,61 @@ function App() {
         My sister is a {familyJob.sister}.
       </h2>
       <h2 style={style}>I have a family of {job +", " + familyJob.father +", " + familyJob.mother + " and " + familyJob.sister}</h2>
+      </div>
+      
    <Hobby name="jashim"></Hobby>
    <Job></Job>
    </div>
     
   );
+}
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  // const handleIncrease = () =>setCount(count + 1);
+  //  console.log('clicked')
+
+  // const newCount = count + 1;
+  // setCount(newCount);
+
+  
+  // const handleDecrease = () =>{
+    //  console.log('clicked')
+  
+    // const newCount = count + 1;
+    // setCount(newCount);
+  
+    // setCount(count - 1);
+  //  };
+
+  return(
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={() =>setCount(count + 1)}>Increase</button>
+      <button onClick={() =>setCount(count - 1)}>Decrease</button>
+    </div>
+  )
+}
+
+function Users(){
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    // console.log('calling effect.')
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+
+  return(
+    <div>
+      <h4>Dynamic User :{users.length}</h4>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
 }
 
 function Hobby(props){
@@ -70,6 +148,27 @@ function Job(){
     }}>
       <h2>Backend web developer.</h2>
       <h2>Front-end Web developer.</h2>
+    </div>
+  )
+}
+
+function Food(props){
+  const foodStyle={
+    border: "1px solid black",
+    borderRadius: "5px",
+    backgroundColor: "lightgray",
+    height: "150px",
+    width: "200px",
+    float: "left",
+    margin: "20px"
+  }
+
+  const {name, price} = props.food;
+  return (
+    <div style={foodStyle}>
+      <h3>{name}</h3>
+      <h3>{price}</h3>
+      <button>Buy Now</button>
     </div>
   )
 }
